29bed41585baf98dfc0fee89c246db7f39f6ff31
Benjamin Wishart <benjamin.wishart@gmail.com>
2026-03-24 23:30:24 -0400

    Fix tests causing build issues

--- src/test/java/com/dotohtwo/review_api/ReviewApiApplicationTests.java
+++ src/test/java/com/dotohtwo/review_api/ReviewApiApplicationTests.java
@@ -1,10 +1,27 @@
 package com.dotohtwo.review_api;
 
+import com.dotohtwo.review_api.repository.ReplyRepository;
+import com.dotohtwo.review_api.repository.ReviewRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.autoconfigure.exclude=" +
+		"org.springframework.boot.cassandra.autoconfigure.CassandraAutoConfiguration," +
+		"org.springframework.boot.data.cassandra.autoconfigure.DataCassandraAutoConfiguration," +
+		"org.springframework.boot.data.cassandra.autoconfigure.CassandraRepositoriesAutoConfiguration"
+)
 class ReviewApiApplicationTests {
+
+	@MockitoBean
+	ReviewRepository reviewRepository;
+
+	@MockitoBean
+	ReplyRepository replyRepository;
+
+	@MockitoBean
+	KafkaTemplate<?, ?> kafkaTemplate;
 
 	@Test
 	void contextLoads() {
