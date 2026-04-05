08794408950da10742b9701362f32a07f9cd8d47
Benjamin Wishart <benjamin.wishart@gmail.com>
2026-04-05 13:28:03 -0400

    Fix build issues

--- src/main/java/com/dotohtwo/review_api/config/RedisConfig.java
+++ src/main/java/com/dotohtwo/review_api/config/RedisConfig.java
@@ -6,20 +6,14 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import tools.jackson.databind.cfg.DateTimeFeature;
-import tools.jackson.databind.json.JsonMapper;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        JsonMapper objectMapper = JsonMapper.builder()
-                .disable(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS)
-                .build();
-
         JacksonJsonRedisSerializer<Object> serializer =
-                new JacksonJsonRedisSerializer<>(objectMapper, Object.class);
+                new JacksonJsonRedisSerializer<>(Object.class);
 
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
